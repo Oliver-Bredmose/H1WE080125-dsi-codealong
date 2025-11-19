96e9541abffeb42ee89b25e66257a01c11eaeb8c
Oliver-Bredmose <olbr1@elev.techcollege.dk>
2025-11-19 14:00:28 +0100

    hejsa

---
+++ index.js
@@ -0,0 +1,10 @@
+import http from "http"
+
+http.createServer((request, Response) => {
+    Response.writeHead(200, {'Content-type': 'text/plain' })
+    Response.end('Hej Webudviklere')
+    console.log('Server with Hello World');
+}).listen(4000, () => {
+    console.log('Server is runnig on http://localhost:4000');
+    
+})
